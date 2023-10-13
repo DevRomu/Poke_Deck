5abde6d5311410c526bc4f494996db574ee18a91
Rhomill <orinarhomill@gmail.com>
2023-10-13 14:53:21 -0500

    Added the pokemon types

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,24 @@ url = URI("https://pokeapi.co/api/v2/pokemon/")
 response = Net::HTTP.get(url)
 json_data = JSON.parse(response)
 
-# iteraate through JSON data and create records in the database
+
+
+# iterate through JSON data and create records in the database
 json_data["results"].each do |pokemon|
+
+  url2 = URI(pokemon["url"])
+  response2 = Net::HTTP.get(url2)
+  json_data2 = JSON.parse(response2)
+
   # create a new record in the database
-  Pokemon.create(name: pokemon["name"], url: pokemon["url"])
+  Pokemon.create(
+    name:      pokemon["name"],
+    image_url: json_data2["sprites"]["front_default"],
+    hp:        json_data2["stats"][0]["base_stat"],
+    attack:    json_data2["stats"][1]["base_stat"],
+    defense:   json_data2["stats"][2]["base_stat"],
+    spatk:     json_data2["stats"][3]["base_stat"],
+    spdef:     json_data2["stats"][4]["base_stat"],
+    speed:     json_data2["stats"][5]["base_stat"]
+  )
 end
