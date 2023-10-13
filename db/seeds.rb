971b39e32c0d8f5ef4543a18cf25edfb1a8a1923
Rhomill <orinarhomill@gmail.com>
2023-10-13 14:34:44 -0500

    Added pokemon api (missing HP, types and moves?)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 #   https://pokeapi.co/api/v2/pokemon/ - Pokemon API
+
+require 'net/http'
+require 'json'
+
+url = URI("https://pokeapi.co/api/v2/pokemon/")
+response = Net::HTTP.get(url)
+json_data = JSON.parse(response)
+
+# iteraate through JSON data and create records in the database
+json_data["results"].each do |pokemon|
+  # create a new record in the database
+  Pokemon.create(name: pokemon["name"], url: pokemon["url"])
+end
