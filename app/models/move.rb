033a8055732c0acc43e5ec99d7c991ea6be8fbd3
xopherv <cjayvergel@gmail.com>
2023-10-17 11:52:01 -0500

    Added validations for Pokemon, Type, and Move tables

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,9 @@
 class Move < ApplicationRecord
   has_many :pokemon_moves
   has_many :pokemons, through: :pokemon_moves
+
+  validates :move_name, presence: true
+  validates :move_name, uniqueness: true
+  validates :power, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 250}
+  validates :accuracy, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 100}
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,4 +3,8 @@ class Pokemon < ApplicationRecord
   has_many :moves, through: :pokemon_moves
   has_many :pokemon_types
   has_many :types, through: :pokemon_types
+
+  validates :pokemon_name, :image_url, :hp, :attack, :defense, :spatk, :spdef, :speed, presence: true
+  validates :pokemon_name, :image_url, uniqueness: true
+  validates :hp, :attack, :defense, :spatk, :spdef, :speed, numericality: {only_integer: true greater_than: 0}
 end
