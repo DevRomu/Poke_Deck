71652f24bd26b5a810af106d072d23baa7cb4bc6
xopherv <cjayvergel@gmail.com>
2023-10-20 14:18:27 -0500

    Changed seed file to include all Pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,7 @@ json_data2["results"].each do |type|
   )
 end
 
-url = URI("https://pokeapi.co/api/v2/pokemon/")
+url = URI("https://pokeapi.co/api/v2/pokemon/?limit=1292")
 response = Net::HTTP.get(url)
 json_data = JSON.parse(response)
 # iterate through JSON data and create records in the database
